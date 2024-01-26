d6286233769a2d451b033c3c6b97ad54dbe3fdd6
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-01-26 20:29:29 +0000

    Got a working navbar and router working

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-function App() {
+import Header from './components/Header';
+import Home from './pages/Home';
+import ProjectsGallery from './pages/ProjectsGallery';
+import React from 'react';
 
+function App() {
   return <Router>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects-gallery" element={<ProjectsGallery />} />
-
     </Routes>
   </Router>
 
