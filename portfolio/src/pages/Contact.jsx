061ea815da1566dd74909f10f90ec357c9c798a9
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-01-28 14:06:33 +0000

    Contact page buttons added with links

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,25 +1,40 @@
+const styles = {
+    contactLink: {
+        boxShadow: '3px 3px 15px rgba(0, 0, 0, 0.4)',
+        fontSize: '1.1rem',
+        fontWeight: '500'
+    }
+}
+
 function Contact() {
     return <div className="pageBorder container-lg text-center">
-        <div className="row mt-5 mb-5">
-            <div className="col-lg-6">
+        <div className="row mt-3 mb-5">
+            <div className="col-lg-6 mb-3">
                 <h2 className="underline pb-3 mb-3">Contact Me</h2>
                 <form>
                     <div className="form-group">
                         <label for="exampleFormControlInput1">Your Name</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="John Doe" />
+                        <input style={styles.contactLink} type="email" className="form-control" id="exampleFormControlInput1" placeholder="Your Name" />
                     </div>
                     <div className="form-group">
                         <label for="exampleFormControlInput1">Subject</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Subject of your Email" />
+                        <input style={styles.contactLink} type="email" className="form-control" id="exampleFormControlInput1" placeholder="Subject of your Message" />
                     </div>
                     <div className="form-group">
                         <label for="exampleFormControlTextarea1">Message</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea style={styles.contactLink} className="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Your Message"></textarea>
                     </div>
                 </form>
             </div>
             <div className="col-lg-6">
                 <h2 className="underline pb-3 mb-3">Places you can find me</h2>
+                <div className="row mx-auto justify-content-center">
+                    <a href="https://www.linkedin.com/in/matthew-lightfoot-5aba00b8" style={styles.contactLink} className="btn btn-light mt-4 mx-3 col-4 d-flex justify-content-around align-items-center" role="button"><img src="./assets/linkedin.svg" alt="" />LinkedIn</a>
+                    <a href="https://github.com/Mtlightfoot" className="btn btn-light mt-4 mx-3 col-4 d-flex justify-content-around align-items-center" style={styles.contactLink} role="button"><img src="./assets/github.svg" alt="" />GitHub</a>
+                    <a href="https://docs.google.com/document/d/1VZxcRjzsV2ZTtb0msm2PhW421wem9iupoWJ9Ka_OO4I/edit?usp=sharing" style={styles.contactLink} className="btn btn-light mt-4 mx-3 col-4 d-flex justify-content-around align-items-center" role="button"><img src="./assets/resume.png" alt="" />Resume</a>
+                    <a href="mailto: m-lightfoot@hotmail.com" style={styles.contactLink} className="btn btn-light mt-4 mx-3 col-4 d-flex justify-content-around align-items-center" role="button"><img src="./assets/email.png" alt="" />Email</a>
+                    <a href="tel:07801963340" style={styles.contactLink} className="btn btn-light mt-4 mx-3 col-4 d-flex justify-content-around align-items-center" role="button"><img src="./assets/phone.svg" alt="" />Phone</a>
+                </div>
             </div>
 
         </div>
--- portfolio/src/pages/ProjectsGallery.jsx
+++ portfolio/src/pages/ProjectsGallery.jsx
@@ -3,7 +3,8 @@ import projects from '../data/projects.json';
 
 function ProjectsGallery() {
     // Body
-    return <div className="pageBorder container-lg">
+    return <div className="pageBorder container-lg text-center">
+    <h2 className="underline pb-3 mb-3 mt-3">My Projects</h2>
         <div className="row mt-5 mb-5 text-center">
             {projects.map((project) => {
                 return <Project key={project.id} title={project.title} description={project.description} skills={project.skills} image={project.image} site={project.site} repo={project.repo}/>;
